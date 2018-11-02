b554cc0c88fe01d5b34794e9884a837ae93b2e45
edv1297 <edv2@williams.edu>
2018-11-01 21:55:16 -0400

    Done with comments for 5

--- q5/Program.fs
+++ q5/Program.fs
@@ -1,6 +1,7 @@
 ﻿open System
 open Parser
 
+
 type Expr =
  | Variable of char
  | Abstraction of char * Expr
@@ -14,31 +15,37 @@ let handleLeft = pleft (pletter) (pchar '.')
 
 let handleMid = pseq handleLeft expr (fun (x,y) -> Abstraction (x,y))
 
+///Remove the parens and make an abstraction out of the insides
 let abstraction : Parser<Expr> = pbetween (pstr "(L") (pchar ')') (handleMid)
 
-//let abstraction : Parser<Expr> = pseq pbetween (pstr "(L") (pchar ')') (handleMid)
+///Remove the parens and make a new application with the insides
+let application: Parser<Expr> = pbetween (pchar '(') (pchar ')') (pseq expr expr (fun (x,y) -> Application (x,y)))
 
-let application: Parser<Expr> = pseq expr expr (fun (x,y) -> Application (x,y))
+/// Order here goes from base case to more flexible cases of exprs
+exprImpl :=  variable <|> abstraction <|> application
 
-exprImpl := variable <|> abstraction <|> application
 
+/// Parse takes in a <param name = "string">lambda calculus string</param> and <returns> and Expr type</returns> and expression as defined above.
 let parse (s: string) : Expr option =
  let x = expr (prepare s)
  match x with
- | Success (x,_) -> Some x
+ | Success (x,_) ->Some x
  | Failure -> None
 
-(*
-let prettyprint (e: Expr) : string =
-  match x with
- | Some x -> x.ToString()
- | None -> "Invalid Program."
-*)
+
+/// A utility function that takes in the output out of parse and represents the abstractions and applications in a <return> string</string>.
+let rec prettyprint (e: Expr) : string =
+  match e with
+  | Variable e -> "Variable("  + e.ToString() + ")"
+  | Abstraction(f,e)-> "Abstraction(Variable(" +  f.ToString() + "), " + prettyprint e + ")"
+  | Application (e,e2) -> "Application(" + prettyprint e + prettyprint e2 + ")"
 
 [<EntryPoint>]
 let main argv =
- let x = argv.[0]
- let y = parse x
- printfn "%A" y
- //printfn "%s" (prettyprint (expr (prepare x)))
- 0
+  let x = argv.[0]
+  let asto = parse x
+  match asto with
+   | Some ast -> printfn "%A" (prettyprint ast)
+   | None -> printfn "Invalid program."
+  0
+  
